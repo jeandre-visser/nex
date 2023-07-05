d7d4bc23966dab6bd159fd79474373b3a609a660
Jeandre Visser <jeandre@nutrimeals.ca>
2023-07-04 21:05:04 -0600

    Add content to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,21 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import styles from './page.module.scss';
+import Hero from 'public/hero.png';
 
 export default function Home() {
   return (
-   <div>
-    Home
-   </div>
-  )
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Enhance Your Digital Product Design</h1>
+        <p className={styles.desc}>
+          We are ready to create the perfect user interface for your website,
+          web app or mobile app.
+        </p>
+        <button className={styles.btn}>Explore Our Work</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} className={styles.img} alt='hero-img' />
+      </div>
+    </div>
+  );
 }
